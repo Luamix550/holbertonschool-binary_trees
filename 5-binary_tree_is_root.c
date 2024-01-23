03215b9f38419c2ee5f536c4417804e4d69aac09
Luamix <luizalfonso611@gmail.com>
2024-01-23 16:47:19 -0500

    binary tree is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_root - that checks if a given node is a root
+ *@node: node to check
+ *Return: node is a root
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+
+	return (node->parent == NULL);
+
+
+}
