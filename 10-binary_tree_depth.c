854496bcbfc94306797c133ca841f5cbef90aef9
Luamix <luizalfonso611@gmail.com>
2024-01-24 19:24:16 -0500

    taks 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- *binary_tree_depth - measures the depth of a node in binary tree
+ *binary_tree_depth - depth of a node in binary tree
  *@tree: pointer to the node to measure the depth
  *Return: depth of the node in a binary tree
 */
@@ -9,9 +9,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	if (!tree)
-	
+
 		return (0);
-	
+
 	if (tree->parent)
 
 		depth += 1 + binary_tree_depth(tree->parent);
