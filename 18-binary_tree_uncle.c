0aab12bb98e05b40a12cfa120293c62a15781a76
Luamix <luizalfonso611@gmail.com>
2024-01-26 11:49:06 -0500

    taks 18 vol 3

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,11 +6,14 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	binary_tree_t *parent;
+    binary_tree_t *grandparent;
+
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
-	binary_tree_t *grandparent = parent->parent;
+	parent = node->parent;
+	grandparent = parent->parent;
 
 	if (grandparent->left == parent)
 		return (grandparent->right);
