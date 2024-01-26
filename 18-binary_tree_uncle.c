ea8b8bab3ceb089a47b6bf306769cf552f82c9b2
Luamix <luizalfonso611@gmail.com>
2024-01-26 11:43:58 -0500

    taks 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle - Finds the uncle of a node in a binary tree
+ * @node: A pointer to the node to find the uncle of
+ *
+ * Return: uncle node, or NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	binary_tree_t *parent = node->parent;
+	binary_tree_t *grandparent = parent->parent;
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
+	else if (grandparent->right == parent)
+		return grandparent->left;
+	else
+		return (NULL);
+}
