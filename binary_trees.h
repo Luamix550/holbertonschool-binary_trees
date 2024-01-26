db5e0f799de89ca5d03dc3513d5464d8bf31cd3f
Luamix <luizalfonso611@gmail.com>
2024-01-26 11:30:37 -0500

    taks 17

--- binary_trees.h
+++ binary_trees.h
@@ -35,11 +35,13 @@ typedef struct binary_tree_s heap_t;
 #include <stddef.h>
 
 /*Prototypes*/
-void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
+void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
